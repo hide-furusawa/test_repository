64d1de5a7fa4bd8c879d1ee3e32710f2d4b95a3a
taro <taro@taronoMacBook-Pro.local>
2019-04-26 18:25:56 +0900

    test

---
+++ app/controllers/samples_controller.rb
@@ -0,0 +1,6 @@
+class SamplesController < ApplicationController
+  def index
+    #render text: 'Hello world'
+    render :text => "Hello, world!"
+  end
+end
---
+++ vendor/bundler/ruby/2.3.0/gems/ffi-1.9.22/ext/ffi_c/libffi-x86_64-darwin16/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/taro/workspace/rails_test/vendor/bundler/ruby/2.3.0/gems/ffi-1.9.22/ext/ffi_c/libffi/src/x86/ffitarget.h
